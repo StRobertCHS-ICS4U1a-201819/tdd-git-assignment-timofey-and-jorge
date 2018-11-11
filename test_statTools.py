6d45401a5a0e1408daf83e5009d855e9ca4cbf7d
HartanT <Th1133033>
2018-11-10 22:42:17 -0500

    create empty test for lower_quartile

--- test_statTools.py
+++ test_statTools.py
@@ -22,6 +22,9 @@ def test_lower_quartile_basic():
 def test_lower_quartile_odd():
     assert(lower_quartile([1,2,3,4,5,6,7]) == 2.5)
 
+def test_lower_quartile_empty():
+    assert(lower_quartile([]) == 0)
+
 def test_merge_basic():
     assert(merge([1,3,5,7,9],[2,4,6,8,10]) == [1,2,3,4,5,6,7,8,9,10])
 
