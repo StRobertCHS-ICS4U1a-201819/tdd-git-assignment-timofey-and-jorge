14e6a55de233d8b07080916852551bea79ad53ff
HartanT <Th1133033>
2018-11-10 21:24:36 -0500

    create merge function and test

--- statTools.py
+++ statTools.py
@@ -2,3 +2,27 @@ def find_range(array):
     lenArr = len(array)
     array_range = array[lenArr-1] - array[0]
     return array_range
+
+def merge(array1, array2):
+    array = []
+    lenArr1 = len(array1)
+    lenArr2 = len(array2)
+    i1 = 0
+    i2 = 0
+
+    while i1 < lenArr1 and i2 < lenArr2:
+        if array1[i1] < array2[i2]:
+            array.append(array1[i1])
+            i1 += 1
+        else:
+            array.append(array2[i2])
+            i2 += 1
+    while i1 < lenArr1:
+        array.append(array1[i1])
+        i1 += 1
+    while i2 < lenArr2:
+        array.append(array2[i2])
+        i2 += 1
+
+    return array
+
--- test_statTools.py
+++ test_statTools.py
@@ -6,3 +6,6 @@ def test_find_range_basic():
 
 def test_find_range_unsorted():
     assert(find_range([3,2,4,1,5]) == 4)
+
+def test_merge_basic():
+    assert(merge([1,3,5,7,9],[2,4,6,8,10]) == [1,2,3,4,5,6,7,8,9,10])
